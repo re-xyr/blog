6aadb4feb0f1034087bb8f6f81d79d1bb997f718
t532 <2037246484@qq.com>
2019-08-09 03:17:59 +0800

    Add info about licensing

--- .vuepress/theme/components/Footer.vue
+++ .vuepress/theme/components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="footer">
         <a href="https://github.com/t532/blog">Built by t532 with ❤</a> / 
-        <a href="https://vuepress.vuejs.org">Powered by VuePress</a>
+        <a href="https://vuepress.vuejs.org">Powered by VuePress</a> /
+        Distributed under MIT License.
     </div>
 </template>
 
