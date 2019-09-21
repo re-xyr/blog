f946f8d18d1a735f3065188b71aa3e5231aa47c0
t532 <2037246484@qq.com>
2019-09-22 04:11:42 +0800

    Add LaTeX support (KaTeX)

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,16 @@
+const MarkdownIt = require('markdown-it')
+const Katex = require('katex')
 module.exports = {
     title: 't532\'s blog',
     description: 'Deadline is motivation',
+    head: [
+        ['link', {
+            rel: 'stylesheet',
+            href: "https://cdn.jsdelivr.net/npm/katex@0.11.0/dist/katex.min.css",
+            integrity: "sha384-BdGj8xC2eZkQaxoQ8nSLefg4AV4/AwB3Fj+8SUSo7pnKP6Eoy18liIKTPn9oBYNG",
+            crossorigin: "anonymous",
+        }],
+    ],
     plugins: [
         [
             '@vuepress/blog', {
@@ -21,4 +31,15 @@ module.exports = {
             },
         ],
     ],
+    markdown: {
+        extendMarkdown(md) {
+            md.render = (src, env) => {
+                return MarkdownIt.prototype.render.call(md, src, env)
+                    .replace(/\$\$(.+?)\$\$/g, (_, str) =>
+                        Katex.renderToString(str, { throwOnError: false, displayMode: true }))
+                    .replace(/\$(.+?)\$/g, (_, str) =>
+                        Katex.renderToString(str, { throwOnError: false, displayMode: false }))
+            }
+        },
+    },
 }
